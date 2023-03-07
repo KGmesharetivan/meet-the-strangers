6c9e7c692c10b5e8c0fdbff1186f3d2afe0b5ef7
Mesharet Ivan Lorica <mesharetivan@gmail.com>
2023-03-08 02:55:52 +0800

    update

--- app.js
+++ app.js
@@ -17,14 +17,62 @@ let connectedPeers = [];
 
 io.on("connection", (socket) => {
   connectedPeers.push(socket.id);
-  console.log(connectedPeers);
+
+  socket.on("pre-offer", (data) => {
+    console.log("pre-offer-came");
+    const { calleePersonalCode, callType } = data;
+    console.log(calleePersonalCode);
+    console.log(connectedPeers);
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === calleePersonalCode
+    );
+
+    console.log(connectedPeer);
+
+    if (connectedPeer) {
+      const data = {
+        callerSocketId: socket.id,
+        callType,
+      };
+      io.to(calleePersonalCode).emit("pre-offer", data);
+    } else {
+      const data = {
+        preOfferAnswer: "CALLEE_NOT_FOUND",
+      };
+      io.to(socket.id).emit("pre-offer-answer", data);
+    }
+  });
+
+  socket.on("pre-offer-answer", (data) => {
+    const { callerSocketId } = data;
+
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === callerSocketId
+    );
+
+    if (connectedPeer) {
+      io.to(data.callerSocketId).emit("pre-offer-answer", data);
+    }
+  });
+
+  socket.on("webRTC-signaling", (data) => {
+    const { connectedUserSocketId } = data;
+
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === connectedUserSocketId
+    );
+
+    if (connectedPeer) {
+      io.to(connectedUserSocketId).emit("webRTC-signaling", data);
+    }
+  });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
 
-    const newConnectedPeers = connectedPeers.filter((peerSocketId) => {
-      peerSocketId !== socket.id;
-    });
+    const newConnectedPeers = connectedPeers.filter(
+      (peerSocketId) => peerSocketId !== socket.id
+    );
 
     connectedPeers = newConnectedPeers;
     console.log(connectedPeers);
