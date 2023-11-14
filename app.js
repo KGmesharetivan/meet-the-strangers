6e2405b449429dc6c2c2ffaaa578f677dc887aa3
mesharetivan <146832484+KGmesharetivan@users.noreply.github.com>
2023-11-14 20:46:10 +0800

    Update

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.get("/", (req, res) => {
 });
 
 let connectedPeers = [];
+let connectedPeersStrangers = [];
 
 io.on("connection", (socket) => {
   connectedPeers.push(socket.id);
@@ -35,7 +36,84 @@ io.on("connection", (socket) => {
         callType,
       };
       io.to(calleePersonalCode).emit("pre-offer", data);
+    } else {
+      const data = {
+        preOfferAnswer: "CALLEE_NOT_FOUND",
+      };
+      io.to(socket.id).emit("pre-offer-answer", data);
     }
+  });
+
+  socket.on("pre-offer-answer", (data) => {
+    const { callerSocketId } = data;
+
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === callerSocketId
+    );
+
+    if (connectedPeer) {
+      io.to(data.callerSocketId).emit("pre-offer-answer", data);
+    }
+  });
+
+  socket.on("webRTC-signaling", (data) => {
+    const { connectedUserSocketId } = data;
+
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === connectedUserSocketId
+    );
+
+    if (connectedPeer) {
+      io.to(connectedUserSocketId).emit("webRTC-signaling", data);
+    }
+  });
+
+  socket.on("user-hanged-up", (data) => {
+    const { connectedUserSocketId } = data;
+
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === connectedUserSocketId
+    );
+
+    if (connectedPeer) {
+      io.to(connectedUserSocketId).emit("user-hanged-up");
+    }
+  });
+
+  socket.on("stranger-connection-status", (data) => {
+    const { status } = data;
+    if (status) {
+      connectedPeersStrangers.push(socket.id);
+    } else {
+      const newConnectedPeersStrangers = connectedPeersStrangers.filter(
+        (peerSocketId) => peerSocketId !== socket.id
+      );
+      connectedPeersStrangers = newConnectedPeersStrangers;
+    }
+
+    console.log(connectedPeersStrangers)
+  });
+
+  socket.on("get-stranger-socket-id", () => {
+    let randomStrangerSocketId;
+    const filterConnectedPeersStrangers = connectedPeersStrangers.filter(
+      (peerSocketId) => peerSocketId !== socket.id
+    );
+
+    if (filterConnectedPeersStrangers.length > 0) {
+      randomStrangerSocketId = 
+        filterConnectedPeersStrangers[
+          Math.floor(Math.random() * filterConnectedPeersStrangers.length)
+      ];
+    } else {
+      randomStrangerSocketId = null;
+    }
+    
+    const data = {
+      randomStrangerSocketId
+    };
+
+    io.to(socket.id).emit("stranger-socket-id", data);
   });
 
   socket.on("disconnect", () => {
@@ -46,7 +124,12 @@ io.on("connection", (socket) => {
     );
 
     connectedPeers = newConnectedPeers;
-    console.log(connectedPeers);
+    //console.log(connectedPeers);
+
+    const newConnectedPeersStrangers = connectedPeersStrangers.filter(
+      (peerSocketId) => peerSocketId !== socket.id
+    );
+    connectedPeersStrangers = newConnectedPeersStrangers;
   });
 });
 
--- public/js/wss.js
+++ public/js/wss.js
@@ -1,6 +1,8 @@
 import * as store from "./store.js";
 import * as ui from "./ui.js";
 import * as webRTCHandler from "./webRTCHandler.js";
+import * as constants from "./constants.js";
+import * as strangerUtils from "./strangerUtils.js";
 
 let socketIO = null;
 
@@ -16,9 +18,57 @@ export const registerSocketEvents = (socket) => {
   socket.on("pre-offer", (data) => {
     webRTCHandler.handlePreOffer(data);
   });
+
+  socket.on("pre-offer-answer", (data) => {
+    webRTCHandler.handlePreOfferAnswer(data);
+  });
+
+  socket.on("user-hanged-up", () => {
+    webRTCHandler.handleConnectedUserHangedUp();
+  })
+
+  socket.on("webRTC-signaling", (data) => {
+    switch (data.type) {
+      case constants.webRTCSignaling.OFFER:
+        webRTCHandler.handleWebRTCOffer(data);
+        break;
+      case constants.webRTCSignaling.ANSWER:
+        webRTCHandler.handleWebRTCAnswer(data);
+        break;
+      case constants.webRTCSignaling.ICE_CANDIDATE:
+        webRTCHandler.handleWebRTCCandidate(data);
+        break;
+      default:
+        return;
+    }
+  });
+
+  socket.on("stranger-socket-id", (data) => {
+    strangerUtils.connectWithStranger(data);
+  })
 };
 
 export const sendPreOffer = (data) => {
   console.log("emmiting to server pre offer event");
   socketIO.emit("pre-offer", data);
+};
+
+export const sendPreOfferAnswer = (data) => {
+  socketIO.emit("pre-offer-answer", data);
+};
+
+export const sendDataUsingWebRTCSignaling = (data) => {
+  socketIO.emit("webRTC-signaling", data);
+};
+
+export const sendUserHangedUp = (data) => {
+  socketIO.emit("user-hanged-up", data);
+};
+
+export const changeStrangerConnectionStatus = (data) => {
+  socketIO.emit("stranger-connection-status", data);
+};
+
+export const getStrangerSocketId = () => {
+  socketIO.emit("get-stranger-socket-id")
 };
